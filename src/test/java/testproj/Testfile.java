c215f902fb6d2f547d2c3bd95e2f9c2b75615e3a
Test User <vmarkandan@INPF0Y7JBF.CSCMWS.CSCMWS.COM>
2021-02-18 20:56:58 +0530

    Changed to run Test file in testng.xml

--- src/test/java/testproj/Testfile.java
+++ src/test/java/testproj/Testfile.java
@@ -1,11 +1,23 @@
 package testproj;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Testfile {
+	
+	static ChromeDriver driver;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("This is a test file");
+
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://www.google.com");
 
 	}
 

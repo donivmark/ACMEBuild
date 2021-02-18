f5aecfc458461d7b28845f15154ba5334eaabe7c
Test User <vmarkandan@INPF0Y7JBF.CSCMWS.CSCMWS.COM>
2021-02-18 20:39:47 +0530

    Push from local after adding test files - 02182021

---
+++ src/test/java/testproj/Testfile.java
@@ -0,0 +1,12 @@
+package testproj;
+
+public class Testfile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is a test file");
+
+	}
+
+}
